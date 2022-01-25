21df8c66f9af9ff946ac0d865eb1e1efc44f2410
Ty Bolt <tybolt@gmail.com>
2022-01-25 16:47:59 -0500

    add file form

--- pages/index.js
+++ pages/index.js
@@ -6,15 +6,32 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Form Demo</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+        <form name="contact" method="POST" data-netlify="true">
+          <p>
+            <label>Your Name: <input type="text" name="name" /></label>   
+          </p>
+          <p>
+            <label>Your Email: <input type="email" name="email" /></label>
+          </p>
+          <p>
+            <label>Message: <textarea name="message"></textarea></label>
+          </p>
+          <p>
+            <span>Add file:</span>
+            <input name="file" type="file"/>
+          <p class="result"></p>
+          
+          </p>
+          <p>
+            <button type="submit">Send</button>
+          </p>
+        </form>
+
       </main>
 
       <Footer />
